e00d180f7932f0997f97537651bf379742069824
oceanli06 <amaliaramadani55@gmail.com>
2024-06-05 10:50:21 +0800

    update nilai data dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -24,25 +24,13 @@ class AdminController extends Controller
 
         $pendingonhold = ProjectList::where('payment_status', 1)->sum('payment') + InstituteProyeks::where('status', 1)->sum('tagihan'); // Ganti 'nilai' dengan nama kolom yang ingin dijumlahkan
         $jumlahyangSudahTerbayar = ProjectList::where('payment_status', 3)->sum('payment') + InstituteProyeks::where('status', 3)->sum('tagihan') + Sales::where('status', 1)->sum('jual'); 
-        $jumlahYangBelumTerbayar = ProjectList::where('payment_status', 1)->sum('payment') + InstituteProyeks::where('status', 1)->sum('tagihan') + Sales::where('status', 0)->sum('jual'); 
+        $jumlahYangBelumTerbayar = ProjectList::where('payment_status', 0)->sum('payment') + InstituteProyeks::where('status', 0)->sum('tagihan') + Sales::where('status', 0)->sum('jual'); 
         $totalProjectExpense = ProjectAktivitis::sum('cost') + InstitutePengeluaran::sum('cost')  + Sales::sum('jual');
         $grossProjectProfit = ProjectList::where('payment_status', 3)->sum('payment') + ProjectList::where('payment_status', 1)->sum('payment') + ProjectList::where('payment_status', 1)->sum('payment') - InstituteProyeks::where('status', 1)->sum('tagihan') + InstituteProyeks::where('status', 3)->sum('tagihan') + InstitutePengeluaran::sum('cost') - Sales::where('status', 1)->sum('jual') + Sales::where('status', 0)->sum('jual') +Sales::sum('jual');
-        // $grossProjectProfit = ProjectList::where('payment_status', 3)->sum('payment') + InstituteProyeks::where('status', 3)->sum('tagihan') + Sales::where('status', 1)->sum('jual') - ProjectList::where('payment_status', 1)->sum('payment') + InstituteProyeks::where('status', 1)->sum('tagihan') + Sales::where('status', 0)->sum('jual') ; 
-        // dd($pendingonhold);
 
         return view('admin.home', compact('totalprojek', 'pending', 'onprogress', 'finish','pendingonhold','jumlahyangSudahTerbayar', 'jumlahYangBelumTerbayar','totalProjectExpense', 'grossProjectProfit'));
     }
 
-    // public function list()
-    // {
-    //     $onhold = ProjectList::sum('nilai') + InstituteProyeks::sum('nilai'); // Ganti 'nilai' dengan nama kolom yang ingin dijumlahkan
-    //     $jumlahyangSudahTerbayar = ProjectList::where('status', 1)->sum('nilai') + InstituteProyeks::where('status', 0)->sum('nilai');
-    //     $jumlahYangBelumTerbayar = ProjectList::where('status', 2)->sum('nilai') + InstituteProyeks::where('status', 1)->sum('nilai');
-    //     $totalProjectExpense = ProjectList::where('status', 3)->sum('nilai') + InstituteProyeks::where('status', 2)->sum('nilai');
-    //     $grossProjectProfit = ProjectList::where('status', 4)->sum('nilai') + InstituteProyeks::where('status', 3)->sum('nilai');
-
-    //     return view('admin.home', compact('onhold', 'jumlahyangSudahTerbayar', 'jumlahYangBelumTerbayar', 'totalProjectExpense', 'grossProjectProfit'));
-    // }
     /**
      * Show the form for creating a new resource.
      */
