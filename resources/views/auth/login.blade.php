6b00abd90d5f7bfea92a0785bbc81fb355780be9
Leonardo12SH <sulaimanhamzah12@gmail.com>
2025-01-24 00:57:43 +0800

    jenkins

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,7 +61,7 @@
                         <p class="text-white text-center">
                             Copyright © <script>
                                 document.write(new Date().getFullYear())
-                            </script> Mell
+                            </script> Andi Amalia Ramadani
                         </p>
                     </div>
                     <div class="col-md-6">
